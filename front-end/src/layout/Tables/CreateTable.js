46fc94c99038229ceeec4d1271aff6050d313093
Chris Dowd <chrisdowd1996@gmail.com>
2022-06-21 20:11:03 -0500

    more cleanup

--- front-end/src/layout/Tables/CreateTable.js
+++ front-end/src/layout/Tables/CreateTable.js
@@ -28,36 +28,33 @@ export default function CreateTable() {
             .catch(setError);
     }
 
+return (
     <div className="card my-3 border-secondary">
-    <h3 className="card-header text-white bg-secondary">Create Table</h3>
-    <div className="card-body"></div>
-        <form onSubmit={handleSubmit}>
-        <div className="col-10 form-group">
-            <label className="form-label" htmlFor="first_name">Table Name: </label>
-            <input 
-                className="form-control" 
-                name="table_name"
-                onChange={handleTable}
-                required
-            />
-            <label className="form-label" htmlFor="last_name">Table Capacity:</label>
-            <input  
-                className="form-control"
-                name="capacity"
-                type="number"
-                required
-                onChange={handleCapacity}
-            /><br/>
-            <div>
-                <button type="button" className="btn btn-secondary m-2" onClick={()=>  history.goBack()}> Cancel </button>
-                <button type="submit" className="btn btn-primary m-2"> Submit </button>
+        <ErrorAlert error={error}/>
+        <h3 className="card-header text-white bg-secondary">Create Table</h3>
+        <div className="card-body">
+            <form onSubmit={handleSubmit}>
+            <div className="col-10 form-group">
+                <label className="form-label" htmlFor="first_name">Table Name: </label>
+                <input 
+                    className="form-control" 
+                    name="table_name"
+                    onChange={handleTable}
+                    required
+                />
+                <label className="form-label" htmlFor="last_name">Table Capacity:</label>
+                <input  
+                    className="form-control"
+                    name="capacity"
+                    type="number"
+                    required
+                    onChange={handleCapacity}
+                />
+                <br/>
+                <ValidateTable tableCapacity={tableCapacity} tableName={tableName} history={history} />
             </div>
+            </form>
         </div>
-        <ValidateTable
-                tableCapacity={tableCapacity}
-                tableName={tableName}
-                history={history}
-            />
-    </form>
-</div>   
+    </div>   
+    )
 }
