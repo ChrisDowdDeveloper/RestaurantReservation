4bbea409660e08c80f7372e825e530c5edb0325f
Chris Dowd <chrisdowd1996@gmail.com>
2022-06-21 20:00:48 -0500

    cleanup

--- front-end/src/layout/Tables/CreateTable.js
+++ front-end/src/layout/Tables/CreateTable.js
@@ -28,33 +28,36 @@ export default function CreateTable() {
             .catch(setError);
     }
 
-
-    return (
-        <div>
-            <ErrorAlert error={error}/>
-            <h1>Create A Table</h1>
+    <div className="card my-3 border-secondary">
+    <h3 className="card-header text-white bg-secondary">Create Table</h3>
+    <div className="card-body"></div>
         <form onSubmit={handleSubmit}>
-            <label>Table Name</label>
-            <input
+        <div className="col-10 form-group">
+            <label className="form-label" htmlFor="first_name">Table Name: </label>
+            <input 
+                className="form-control" 
                 name="table_name"
                 onChange={handleTable}
                 required
             />
-            <br />
-            <label>Capacity</label>
-            <input
+            <label className="form-label" htmlFor="last_name">Table Capacity:</label>
+            <input  
+                className="form-control"
                 name="capacity"
                 type="number"
                 required
                 onChange={handleCapacity}
-            />
-            <br />
-            <ValidateTable
+            /><br/>
+            <div>
+                <button type="button" className="btn btn-secondary m-2" onClick={()=>  history.goBack()}> Cancel </button>
+                <button type="submit" className="btn btn-primary m-2"> Submit </button>
+            </div>
+        </div>
+        <ValidateTable
                 tableCapacity={tableCapacity}
                 tableName={tableName}
                 history={history}
             />
-        </form>
-        </div>
-    )
+    </form>
+</div>   
 }
