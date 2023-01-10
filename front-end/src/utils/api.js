457d4e25154886e2769583feda6eca5b5fcf3edc
Chris Dowd <91644551+ChrisDowdDeveloper@users.noreply.github.com>
2023-01-10 08:39:02 -0600

    new backend

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://periodicrestaurant-server.herokuapp.com";
+  process.env.REACT_APP_API_BASE_URL || "https://restuarantreservation.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with a `json-server`
