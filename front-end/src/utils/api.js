5b4f0ac8d256f6e2fc872f904ea8c1aac6a796cc
Chris Dowd <91644551+ChrisDowdDeveloper@users.noreply.github.com>
2023-01-10 10:24:28 -0600

    db issues

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://restuarant-reservation-backend-f4n4.onrender.com";
+  process.env.REACT_APP_API_BASE_URL || "https://restaurantreservation-backend.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with a `json-server`
