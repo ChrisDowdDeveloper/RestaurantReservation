3906bdfeb719255ee94911ad25b46e74f052bb3b
Chris Dowd <91644551+ChrisDowdDeveloper@users.noreply.github.com>
2023-01-12 10:06:50 -0600

    CORS

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -12,6 +12,15 @@ const tablesRouter = require("./tables/tables.router")
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://restaurantreservation.onrender.com/");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use(cors());
 app.use(express.json());
 
